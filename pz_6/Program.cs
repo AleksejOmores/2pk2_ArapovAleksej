e23e3da2286f70c88b25c617d29b1238d3f50f76
OmoreCat <aleksejsoroh@gmail.com>
2022-10-06 21:25:19 +0500

    pz_6 add

---
+++ pz_6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double p = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            int i = 0;
+            while (x < y)
+            {
+                x *= 1 + p / 100;
+                x = (100 * x) / 100;
+                i += 1;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
