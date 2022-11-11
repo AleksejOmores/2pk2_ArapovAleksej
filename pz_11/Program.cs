566c225a2c71177aae1fda8d7b0c48d4b7582278
AleksejOmores <aleksejsoroh@gmail.com>
2022-11-11 21:29:54 +0500

    pz_11 add

---
+++ pz_11/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace pz_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int stringlen = rnd.Next(1, 100);
+            string str = "";
+            char letter;
+            for (int i = 0; i < stringlen; i++)
+            {
+                letter = Convert.ToChar(rnd.Next(0, 100));
+                str = str + letter;
+            }
+            Console.WriteLine(str);
+            Console.WriteLine();
+
+            Console.WriteLine("Символов в тексте " + str.Length);
+            Console.WriteLine();
+
+            string result;
+
+            if (str.Length % 2 == 0)
+            {
+                result = Convert.ToString(str.Substring(str.Length / 2, 1));
+                Console.WriteLine("Количество четное, поэтому один выводим символ из центре: " + result);
+            }
+            else
+            {
+                result = Convert.ToString(str.Substring(str.Length / 2, 2));
+                Console.WriteLine("Количество не четное, поэтому выводим два символа из центре: " + result);
+            }
+        }
+    }
+}
