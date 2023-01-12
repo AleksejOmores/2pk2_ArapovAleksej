44096b5cdc04530e7501cca387e832480163ed79
AleksejOmores <aleksejsoroh@gmail.com>
2023-01-12 11:19:53 +0500

    pz_13 add

---
+++ pz_13/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace pz_13
+{
+    internal class Program
+    {
+        static string StrArr(string a)
+        {
+            string text = null;
+            string result = null;
+            string[] Str = a.Split(' ');
+            for (int i = 0; i < Str.Length; i++)
+            {
+                text = Str[i].Substring(0, 1).ToUpper();
+                if (i == 0)
+                {
+                    result = text + Str[i].Substring(1) + " ";
+                }
+                else
+                {
+                    result += text + ".";
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine($"\"{StrArr(text)}\"");
+        }
+    }
+}
