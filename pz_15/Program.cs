5961aa378a6996c45f04509f5e10d0ae73f50c1c
AleksejOmores <aleksejsoroh@gmail.com>
2023-01-12 11:23:23 +0500

    pz_15 add

---
+++ pz_15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace pz_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file1 = @"E:\inFile.txt";
+            FileInfo file = new FileInfo(file1);
+            if (!file.Exists)
+            {
+                file.Create();
+            }
+
+            Console.WriteLine("Введите текст: ");
+
+            using (StreamWriter sw = new StreamWriter(file1))
+            {
+                string line = "";
+                line = Console.ReadLine();
+                sw.WriteLine(line);
+                string digits = Regex.Replace(line, @"\D", "");
+                Console.WriteLine("Количество цифр: " + digits.Length);
+                sw.Close();
+                Console.ReadLine();
+            }
+            using (StreamReader sr = new StreamReader(file1))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+    }
+}
