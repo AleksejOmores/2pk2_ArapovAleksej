3b469ce21b3e92b2b5d85509f29427eed47c990d
AleksejOmores <aleksejsoroh@gmail.com>
2023-01-12 11:08:21 +0500

    pz_6 add

---
+++ pz_6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace pz_6;
+class Program
+{ 
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double p = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            int i = 0;
+            while (x < y)
+            {
+                x *= 1 + p / 100;
+                x = (100 * x) / 100;
+                i += 1;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
