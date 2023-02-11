9b3f6ff962cdac0b265c0cd9c80b22d34666fa9c
AleksejOmores <aleksejsoroh@gmail.com>
2023-02-11 10:32:53 +0500

    pz_22 add

---
+++ pz_22/Phone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_22
+{
+    internal class Phone
+    {
+        public string Prefix { get; set; }
+        public string Code { get; set; }
+        public string Number { get; set; }
+        public string Type { get; set; }
+        public static int const1 = 0;
+        public static int const2 = 0;
+
+        public static void GetPhoneNumber(ref string Prefix, ref string Code, ref string Number)
+        {
+            if(Prefix == "+1")
+            {
+                Console.WriteLine($"Введено корпоративных номеров: {const1} ");
+                Console.WriteLine($"{Prefix}({Code}){Number}");
+            }
+            if (Prefix == "+7")
+            {
+                Console.WriteLine($"Введено личных номеров: {const2} ");
+                Console.WriteLine($"{Prefix}({Code}){Number}");
+            }
+            
+        }
+    }
+}
---
+++ pz_22/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace pz_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool correct = true;
+            string phoneFormat = "###-##-##";
+            Phone phone = new Phone();
+            while (correct)
+            {
+                Console.Write("Какой номер вам нужен? Корпоративный(1) или личный(2)?: ");
+                phone.Type = Console.ReadLine();
+
+                if (phone.Type == "1")
+                {
+                    string one = phone.Prefix = "+1";
+                    Console.Write("Введите первые 3 цифры: ");
+                    string two = phone.Code = Console.ReadLine();
+                    Console.Write("Введите последующие 7 цифр: ");
+                    string three = phone.Number = Convert.ToInt32(Console.ReadLine()).ToString(phoneFormat);
+                    Console.WriteLine();
+                    Phone.const1++;
+                    Phone.GetPhoneNumber(ref one, ref two, ref three);
+                    correct = false;
+                }
+                else if (phone.Type == "2")
+                {
+                    
+                    string one = phone.Prefix = "+7";
+                    Console.Write("Введите первые 3 цифры: ");
+                    string two = phone.Code = Console.ReadLine();
+                    Console.Write("Введите последующие 7 цифр: ");
+                    string three = phone.Number = Convert.ToInt32(Console.ReadLine()).ToString(phoneFormat);
+                    Console.WriteLine();
+                    Phone.const2++;
+                    Phone.GetPhoneNumber(ref one, ref two, ref three);
+                    correct = false;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            }
+        }
+
+    }
+}
