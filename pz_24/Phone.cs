e97d1437bfdb386e2a66add02adbe8b76a509c02
AleksejOmores <aleksejsoroh@gmail.com>
2023-02-24 14:22:25 +0500

    pz_24 add

---
+++ pz_24/Phone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_24
+{
+    class Phone : ICloneable
+    {
+        public string Prefix { get; set; }
+        public string Code { get; set; }
+        public string Number { get; set; }
+        public string Type { get; set; }
+        public Phone(string prefix, string code, string number, string type)
+        {
+            Prefix = prefix;
+            Code = code;
+            Number = number;
+            Type = type;
+        }
+        public object Clone() => MemberwiseClone();
+
+
+        public static int const1 = 0;
+        public static int const2 = 0;
+
+        public virtual void GetPhoneNumber(ref string Prefix, ref string Code, ref string Number)
+        {
+            if (Prefix == "+1")
+            {
+                Console.WriteLine($"Введено корпоративных номеров: {const1} ");
+                Console.WriteLine($"{Prefix}({Code}){Number}");
+            }
+            if (Prefix == "+7")
+            {
+                Console.WriteLine($"Введено личных номеров: {const2} ");
+                Console.WriteLine($"{Prefix}({Code}){Number}");
+            }
+        }
+    }
+    internal class PrivatePhone : Phone 
+    {
+
+        public List<string> Availabilities = new List<string>() { "Клиент", "Рабочий", "Начальник", "Сотрудник" };
+
+        public PrivatePhone(string prefix, string code, string number, string type) : base(prefix, code, number, type)
+        {
+        }
+
+        public override void GetPhoneNumber(ref string Prefix, ref string Code, ref string Number)
+        {
+            base.GetPhoneNumber(ref Prefix, ref Code, ref Number);
+        }
+    }
+
+}
---
+++ pz_24/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Numerics;
+
+namespace pz_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivatePhone Privat = new PrivatePhone();
+            bool correct = true;
+            string phoneFormat = "###-##-##";
+            Phone phone = new Clone();
+            while (correct)
+            {
+                Console.Write("Какой номер вам нужен? Корпоративный(1) или личный(2)?: ");
+                phone.Type = Console.ReadLine();
+
+                if (phone.Type == "1")
+                {
+                    Console.WriteLine("Кем вы являетесь?");
+                    string Contact = Console.ReadLine();
+                    bool hasContact = Privat.Availabilities.Contains(Contact);
+                    if ((Contact == "Клиент" || Contact == "Рабочий") && hasContact == true)
+                    {
+                        Console.WriteLine("Доступ предоставлен!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не можем предоставить");
+                        return;
+                    }
+                    Console.WriteLine();
+                    string one = phone.Prefix = "+1";
+                    Console.Write("Введите первые 3 цифры: ");
+                    string two = phone.Code = Console.ReadLine();
+                    Console.Write("Введите последующие 7 цифр: ");
+                    string three = phone.Number = Convert.ToInt32(Console.ReadLine()).ToString(phoneFormat);
+                    Console.WriteLine();
+                    Phone.const1++;
+                    Privat.GetPhoneNumber(ref one, ref two, ref three);
+                    correct = false;
+                }
+                else if (phone.Type == "2")
+                {
+                    Console.WriteLine("Кем вы являетесь?");
+                    string Contact = Console.ReadLine();
+                    bool hasContact = Privat.Availabilities.Contains(Contact);
+                    if ((Contact == "Начальник" || Contact == "Сотрудник") && hasContact == true)
+                    {
+                        Console.WriteLine("Доступ предоставлен!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не можем предоставить");
+                        return;
+                    }
+                    Console.WriteLine();
+                    string one = phone.Prefix = "+7";
+                    Console.Write("Введите первые 3 цифры: ");
+                    string two = phone.Code = Console.ReadLine();
+                    Console.Write("Введите последующие 7 цифр: ");
+                    string three = phone.Number = Convert.ToInt32(Console.ReadLine()).ToString(phoneFormat);
+                    Console.WriteLine();
+                    Phone.const2++;
+                    Privat.GetPhoneNumber(ref one, ref two, ref three);
+                    correct = false;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            }
+        }
+
+    }
+}
