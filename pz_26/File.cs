4794520b0fa1f471108b5b25760ddc66f3a7cce6
Алексей Арапов <aleksejsoroh@gmail.com>
2023-03-16 13:19:49 +0500

    pz_26 add

---
+++ pz_26/File.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_26
+{
+    internal class File
+    {
+        string path = @"C:\Users\arapov.ad1680\source\repos\2pk2_ArapovAleksej\pz_26\data";
+        FileStream file1 = new FileStream($@"C:\Users\arapov.ad1680\source\repos\2pk2_ArapovAleksej\pz_26\data\new_file.txt", FileMode.Create);
+    }
+}
+// https://www.cyberforum.ru/csharp-beginners/thread2202891.html?ysclid=lfatiyaqfk251558561
