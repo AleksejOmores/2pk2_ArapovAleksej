09e73f6001aebd511190e3fa00b7def578d5f80b
AleksejOmores <aleksejsoroh@gmail.com>
2023-04-01 16:01:43 +0500

    pz_27 add

---
+++ pz_27/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace pz_27
+{
+    struct ZNAK
+    {
+        public string NAME;
+        public string ZODIAC;
+        public int[] BDAY;
+        public ZNAK(string name, string zodiak, int[] bday)
+        {
+            NAME = name;
+            ZODIAC = zodiak;
+            BDAY = new int[bday.Length];
+            BDAY = bday;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ZNAK[] book = new ZNAK[8];
+
+            
+        }
+    }
+}
