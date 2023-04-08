ff7f2cb22d08232a21b3144c11784c8dd6b71a41
AleksejOmores <aleksejsoroh@gmail.com>
2023-04-08 13:26:44 +0500

    08.04 add

---
+++ 08.04/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _08._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sphere us1 = new Sphere(4, 0);
+            Thread.Sleep(100);
+            Sphere us2 = new Sphere(3, 4);
+
+            Console.WriteLine(us1 + "\n" + us2);
+            Console.WriteLine(us1 < us2);
+        }
+    }
+}
---
+++ 08.04/Sphere.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._04
+{
+    internal class Sphere
+    {
+        public int Radius { get; set; }
+        public int Values { get; set; }
+
+        public Sphere(int n, int v)
+        {
+            Radius = n;
+            Values = v;
+        }
+
+        public override string ToString()
+        {
+            return new string($"Радиус: {Radius} Объем {Values}");
+        }
+
+        public static bool operator >(Sphere u1, Sphere u2)
+        {
+            if (u1.Radius > u2.Values) return true;
+            else return false;
+        }
+
+        public static bool operator <(Sphere u1, Sphere u2)
+        {
+            if (u1.Radius < u2.Values) return true;
+            else return false;
+        }
+    }
+}
