b16f5c1bef4cd118285329d4bcb872be10fbf103
AleksejOmores <aleksejsoroh@gmail.com>
2023-04-09 18:25:46 +0500

    pz_27 unfinished

--- pz_27/Program.cs
+++ pz_27/Program.cs
@@ -1,26 +1,52 @@
 ﻿namespace pz_27
 {
-    struct ZNAK
+    class ZNAK
     {
         public string NAME;
         public string ZODIAC;
-        public int[] BDAY;
-        public ZNAK(string name, string zodiak, int[] bday)
-        {
-            NAME = name;
-            ZODIAC = zodiak;
-            BDAY = new int[bday.Length];
-            BDAY = bday;
-        }
+        public int[] BDAY = new int[3];
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            ZNAK[] book = new ZNAK[8];
+            Random rnd = new Random();
 
+            ZNAK znk = new ZNAK();
+            ZNAK[] BOOK = new ZNAK[2];
             
+            for (int i = 0; i < BOOK.Length; i++)
+            {
+                int x = rnd.Next(1, 30);
+                int y = rnd.Next(1, 12);
+                int z = rnd.Next(1988, 2023);
+                Console.WriteLine("Введите данные {0}:", i + 1);
+
+                Console.Write("Напишите Имя и Фамилию: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Напишите свой задиак: ");
+                string zodiac = Console.ReadLine();
+
+                Console.WriteLine("Дата Рождения: ");
+
+                BOOK[i] = new ZNAK { NAME = name, ZODIAC = zodiac };
+            }
+            Console.WriteLine("Введите имя или фамилию: ");
+            string found = Console.ReadLine();
+
+            foreach (ZNAK znak in BOOK)
+            {
+                if (znak.NAME == found)
+                {
+                    Console.WriteLine($"Вас зовут: {znak.NAME} Ваш знак Зодиака: {znak.ZODIAC} Вы родились: {znak.BDAY}");
+                }
+                if (znak.NAME != found)
+                {
+                    Console.WriteLine($"Ваше Имя/Фамилия:{found} не были найдены в базе данных");
+                }
+            }
         }
     }
 }
