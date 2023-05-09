6b7c6cae2898cfb4f2c97958e4ea61f030f2051d
AleksejOmores <aleksejsoroh@gmail.com>
2023-05-09 18:40:04 +0500

    pz_29 not add

---
+++ BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+        public AccountEventArgs(string v, decimal sum)
+        {
+            V = v;
+            Sum = sum;
+        }
+
+        public string V { get; }
+        public decimal Sum { get; }
+        public bool Message { get; set; }
+    }
+}
